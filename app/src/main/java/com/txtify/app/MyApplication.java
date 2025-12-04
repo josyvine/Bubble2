a0148942e71e5f336f487a2efdf5468054d239b9
josyvine <64621103+josyvine@users.noreply.github.com>
2025-12-05 05:04:10 +0530

    Update MyApplication.java

--- app/src/main/java/com/txtify/app/MyApplication.java
+++ app/src/main/java/com/txtify/app/MyApplication.java
@@ -3,9 +3,15 @@ package com.txtify.app;
 import android.content.Context;
 import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 // This is the definitive, corrected version for MultiDex support in AIDE.
 public class MyApplication extends MultiDexApplication {
+
+    // NEW: Reference to the AppOpenManager
+    private AppOpenManager appOpenManager;
 
     // This method is called BEFORE onCreate(). It's the safest place to install MultiDex.
     @Override
@@ -26,5 +32,16 @@ public class MyApplication extends MultiDexApplication {
         // This line sets up our crash handler. It will now be activated
         // after the critical MultiDex setup is complete.
         Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(this));
+
+        // NEW: Initialize the Google Mobile Ads SDK
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                // Ads SDK initialized successfully
+            }
+        });
+
+        // NEW: Initialize the AppOpenManager to handle "Reopen" ads
+        appOpenManager = new AppOpenManager(this);
     }
 }
